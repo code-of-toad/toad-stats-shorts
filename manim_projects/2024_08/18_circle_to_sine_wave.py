8ba54fc00b8ba08254e75defc976599513773451
Toad <codeoftoad@gmail.com>
2024-08-18 17:38:06 -0400

    Create first project

---
+++ manim_projects/2024_08/18_circle_to_sine_wave.py
@@ -0,0 +1,29 @@
+from manim import *
+
+"""
+Uncomment the next 4 lines for vertical resolution.
+"""
+config.pixel_width  = 1080
+config.pixel_height = 1920
+config.frame_width  = 4.5
+config.frame_height = 8.0
+
+class CircleToSineWave(Scene):
+    def construct(self):
+        # ------------------------------ CONFIG -------------------------------
+        self._config(bg_color=BLACK, grid=False)
+        # ---------------------------------------------------------------------
+        pass
+
+    def _config(self, bg_color='#131313', grid=True):
+        self.camera.background_color = bg_color
+        if grid:
+            self.add(NumberPlane(
+                background_line_style={
+                    "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
+                },
+                axis_config={
+                    "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
+                }
+            ))
+
