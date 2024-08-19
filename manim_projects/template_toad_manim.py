0c19c6ad7f4c6828ea18b92311879efe6ca8b863
Toad <codeoftoad@gmail.com>
2024-08-19 00:44:50 -0400

    Update manim template

--- manim_projects/template_toad_manim.py
+++ manim_projects/template_toad_manim.py
@@ -1,5 +1,6 @@
 from manim import *
 
+
 """
 Uncomment the next 4 lines for vertical resolution.
 """
@@ -11,19 +12,28 @@ Uncomment the next 4 lines for vertical resolution.
 class ToadTemplate(Scene):
     def construct(self):
         # ------------------------------ CONFIG -------------------------------
-        self._config()
+        self._config(grid=True, screen_border=False)
         # ---------------------------------------------------------------------
         pass
 
-    def _config(self, bg_color='#131313', grid=True):
+    def _config(self, bg_color='#131313', grid=True, screen_border=False):
         self.camera.background_color = bg_color
         if grid:
             self.add(NumberPlane(
+                x_range=[-config.frame_width/2,  config.frame_width/2,  1],
+                y_range=[-config.frame_height/2, config.frame_height/2, 1],
                 background_line_style={
                     "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
                 },
                 axis_config={
                     "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
                 }
+            ))
+        if screen_border:
+            self.add(Rectangle(
+                width =config.frame_width,
+                height=config.frame_height,
+                stroke_color=GREEN,
+                stroke_width=20
             ))
 
