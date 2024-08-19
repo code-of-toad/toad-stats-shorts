76f135b53ec8201eb85120853e47eb5fc630de79
Toad <codeoftoad@gmail.com>
2024-08-19 18:47:32 -0400

    Learn: Adding sounds effects

--- manim_projects/practice_files/basic_collision.py
+++ manim_projects/practice_files/basic_collision.py
@@ -11,12 +11,12 @@ config.frame_height = 16.0
 class BasicCollision(Scene):
     def construct(self):
         # ------------------------------ CONFIG -------------------------------
-        self._config(grid=True, screen_border=False)
+        # self._config(grid=True, screen_border=False)
         # ---------------------------------------------------------------------
         width, height = 6, 9
         box = Rectangle(width=width, height=height, stroke_width=6)
-        ball = Dot(color=GREEN, radius=0.25, stroke_width=7)
-        ball.dx, ball.dy = 0.1, 0.1
+        ball = Dot(color=GREEN, radius=0.2, stroke_width=7)
+        ball.dx, ball.dy = 0.15, 0.15
 
         def collision_update(ball):
             pt_right  = ball.get_right()[0]
@@ -25,14 +25,16 @@ class BasicCollision(Scene):
             pt_bottom = ball.get_bottom()[1]
             if pt_right >= width/2 or pt_left <= -width/2:
                 ball.dx *= -1
+                self.add_sound('./assets/audio/bop.wav')
             if pt_top >= height/2 or pt_bottom <= -height/2:
                 ball.dy *= -1
-            ball.shift(ball.dx*RIGHT + ball.dy*UP)
+                self.add_sound('./assets/audio/bop.wav')
+            ball.shift(ball.dx*RIGHT, ball.dy*UP)
 
         self.play(Create(box))
         self.play(Create(ball))
         ball.add_updater(collision_update)
-        self.play(UpdateFromFunc(ball, lambda _: None), run_time=7)
+        self.play(UpdateFromFunc(ball, lambda _: None), run_time=10)
         self.wait()
 
     def _config(self, bg_color='#131313', grid=True, screen_border=False):
