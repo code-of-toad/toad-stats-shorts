9286f595fdc879f1637111c7e4a2dbddc6dbd93a
Toad <codeoftoad@gmail.com>
2024-08-21 01:05:31 -0400

    Create animation_types.py

---
+++ manim_projects/practice_files/animation_types.py
@@ -0,0 +1,38 @@
+from manim import *
+"""
+Uncomment the next 4 lines for vertical resolution.
+"""
+config.pixel_width  = 1080
+config.pixel_height = 1920
+config.frame_width  = 9.0
+config.frame_height = 16.0
+
+
+class ToadTemplate(Scene):
+    def construct(self):
+        # ------------------------------ CONFIG -------------------------------
+        self._config(grid=True, screen_border=False)
+        # ---------------------------------------------------------------------
+        pass
+
+    def _config(self, bg_color='#131313', grid=True, screen_border=False):
+        self.camera.background_color = bg_color
+        if grid:
+            self.add(NumberPlane(
+                x_range=[-config.frame_width/2,  config.frame_width/2,  1],
+                y_range=[-config.frame_height/2, config.frame_height/2, 1],
+                background_line_style={
+                    "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
+                },
+                axis_config={
+                    "stroke_color": WHITE, "stroke_width": 1, "stroke_opacity": 0.1
+                }
+            ))
+        if screen_border:
+            self.add(Rectangle(
+                width =config.frame_width,
+                height=config.frame_height,
+                stroke_color=GREEN,
+                stroke_width=20
+            ))
+
